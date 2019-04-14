3e6fd956c4af7ea368d87a08b8e2319f4be5fc15
Justin van Elst <justinvanelst@gmail.com>
2019-04-14 10:16:39 +0000

    Auto translate 04/14/2019 10:16:39

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'magentotwotranslations_tl_ph',
+    __DIR__
+);
